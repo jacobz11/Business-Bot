27e8ab693f8ee1f0f51f2e8c3ccf2d48f73bca3a
jacobz11 <101628350+jacobz11@users.noreply.github.com>
2024-12-12 18:05:28 +0200

    Update2 bot_python.py

--- bot_python.py
+++ bot_python.py
@@ -35,5 +35,5 @@ if __name__ == "__main__":
     bot_thread.start()
 
     # Start the Flask server
-    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned PORT
+    port = int(os.environ.get("PORT", 10000))  # Use Render's assigned PORT
     app.run(host="0.0.0.0", port=port)
