320a8a22a2de373392a8a89dfe10bcb2ea4c231a
jacobz11 <101628350+jacobz11@users.noreply.github.com>
2024-12-15 15:06:38 +0200

    Update55 bot_python.py

--- bot_python.py
+++ bot_python.py
@@ -19,20 +19,3 @@ def question(message):
     bot.send_message(message.chat.id, "שלום, אני הבוט של חגית בן פורת. איך אוכל לסייע?🙂", reply_markup=markup)#1
 
 bot.polling()
-
-# # Simple Flask server setup
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return "Bot is running!"
-
-# if __name__ == "__main__":
-#     import threading
-#     # Run the bot in a separate thread
-#     bot_thread = threading.Thread(target=run_bot)
-#     bot_thread.start()
-
-#     # Start the Flask server
-#     port = int(os.environ.get("PORT", 10000))  # Use Render's assigned PORT
-#     app.run(host="0.0.0.0", port=port)
